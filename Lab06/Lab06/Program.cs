0a048f8be64eed0fba7ea1e05025d1c5a26f04ef
ศรัญญู ตำลิ่ม <deathnote_boat@hotmail.com>
2017-11-07 17:37:41 +0700

    private

--- Lab06/Lab06/Program.cs
+++ Lab06/Lab06/Program.cs
@@ -18,9 +18,9 @@ namespace ConsoleApp5
     }
     class Student
     {
-        public string Name;
-        public string ID;
-        public float GPA;
+        private string Name;
+        private string ID;
+        private float GPA;
     }
 
 }
